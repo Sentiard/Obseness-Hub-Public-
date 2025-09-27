f2d2fefb774d860ff26070ceac08b2896ebbf8e8
Sentiard <vcxznum777@gmail.com>
2025-09-27 22:47:07 +0900

    Obseness Hub.lua

--- Obseness Hub.lua
+++ Obseness Hub.lua
@@ -1,6 +1,5 @@
 -- Settings
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
-local OHVersion = "V1.3"
 
 local Window = Rayfield:CreateWindow({
    Name = "Obseness Hub",
@@ -134,4 +133,5 @@ local Button = Tab2:CreateButton({
 local Label = Tab3:CreateLabel("Made By. Obseness", 4483362458, Color3.fromRGB(150, 150, 150), false) -- Title, Icon, Color, IgnoreTheme
 
 -- Version
-local Paragraph = Tab4:CreateParagraph({Title = "Date. 2025-09-27", Content = OHVersion})
+local Paragraph = Tab4:CreateParagraph({Title = "Date. 2025-09-27", Content = "V1.4"})
+
